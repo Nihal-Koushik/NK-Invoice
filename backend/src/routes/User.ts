a78fd5d54ed412834f78a1b137b36b24e10eeb49
Arbaaz Laskar <arzkar.dev@gmail.com>
2024-08-04 00:49:54 +0530

    refactor: fix the issue where the routes were not accesible

--- backend/src/routes/User.ts
+++ backend/src/routes/User.ts
@@ -1,56 +1,60 @@
-import User from '../models/user';
-import { app } from './index'
+import User from "../models/User";
+import express, { Request, Response } from "express";
 
-app.get('/user', async (req: Request, res: Response) => {
+const router = express.Router();
 
-    //const token = req.headers.authorization?.replace('Bearer ', '') || '';
-    //const userId = getUserIdFromToken(token);
+router.get("/", async (req: Request, res: Response) => {
+  //const token = req.headers.authorization?.replace('Bearer ', '') || '';
+  //const userId = getUserIdFromToken(token);
 
-    // if (!userId) {
-    //     return res.status(401).json({ error: 'Unauthorized' });
-    // }
-    const users = await User.findAll();
-    // res.json(users);
-    res.status(201).json(users);
+  // if (!userId) {
+  //     return res.status(401).json({ error: 'Unauthorized' });
+  // }
+  const users = await User.findAll();
+  // res.json(users);
+  res.status(201).json(users);
 });
 
-app.post('/user', async (req: Request, res: Response) => {
-    const { username, password, email, mobileNumber } = req.body;
+router.post("/", async (req: Request, res: Response) => {
+  console.log("req.body", req.body);
+  const { username, password, email, mobileNumber } = req.body;
 
-    try {
-        const user = await User.create({ username, password, email, mobileNumber, isActive: true });
-        res.status(201).json(user);
-    } catch (error) {
-        console.error(error); // Log the error for debugging
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const user = await User.create({
+      username,
+      password,
+      email,
+      mobileNumber,
+      isActive: true,
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-
-app.put('/todos/:id', async (req: Request, res: Response) => {
-    const { id } = req.params;
-
-    const token = req.headers.authorization?.replace('Bearer ', '') || '';
-    const userId = getUserIdFromToken(token);
-
-    if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const { title, completed } = req.body;
-    await Todo.update({ title, completed }, { where: { id: id, userId: userId } });
-    res.json({ message: 'Todo updated successfully' });
+router.put("/", async (req: Request, res: Response) => {
+  // const { id } = req.params;
+  // const token = req.headers.authorization?.replace('Bearer ', '') || '';
+  // const userId = getUserIdFromToken(token);
+  // if (!userId) {
+  //     return res.status(401).json({ error: 'Unauthorized' });
+  // }
+  // const { title, completed } = req.body;
+  // await Todo.update({ title, completed }, { where: { id: id, userId: userId } });
+  // res.json({ message: 'Todo updated successfully' });
 });
 
-app.delete('/todos/:id', async (req: Request, res: Response) => {
-    const { id } = req.params;
-
-    const token = req.headers.authorization?.replace('Bearer ', '') || '';
-    const userId = getUserIdFromToken(token);
-
-    if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-    }
-    await Todo.destroy({ where: { id: id, userId: userId } });
-    res.json({ message: 'Todo deleted successfully' });
+router.delete("/", async (req: Request, res: Response) => {
+  // const { id } = req.params;
+  // const token = req.headers.authorization?.replace('Bearer ', '') || '';
+  // const userId = getUserIdFromToken(token);
+  // if (!userId) {
+  //     return res.status(401).json({ error: 'Unauthorized' });
+  // }
+  // await Todo.destroy({ where: { id: id, userId: userId } });
+  // res.json({ message: 'Todo deleted successfully' });
 });
+
+export default router;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,22 +1,24 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import sequelize from '../db';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import sequelize from "../db";
+import userRoutes from "./User";
 
 export const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-// middleware to log incoming requests 
+// middleware to log incoming requests
 app.use((req, res, next) => {
-    console.log(`\n${req.method} ${req.url}`);
-    next();
+  console.log(`\n${req.method} ${req.url}`);
+  next();
 });
 
-
+// Use the routes
+app.use("/user", userRoutes);
 
 sequelize.sync().then(() => {
-    app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
 });
